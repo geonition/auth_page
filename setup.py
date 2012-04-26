060819faaec651c9ab274279d3922a46bd20dd30
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-04-26 12:11:17 +0300

    Version 4.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='auth_page',
-    version='1.0.0',
+    version='4.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/auth_page',
     packages=find_packages(),
